fd7fe3988cb0cf83ec7d89756678c5888addf60f
Dawid Pura <puradawid@gmail.com>
2025-03-15 20:36:07 +0100

    Extended thesis plan

---
+++ rozszerzony_konspekt/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Run \"build\" or \"docker\""
+
+if [[ $1 == "build" ]]; then
+	 docker run --rm -u $(id -u):$(id -g) -v $PWD:/data latex-machine make
+fi
+
+if [[ $1 == "docker" ]]; then
+	docker build -t latex-machine .
+fi
